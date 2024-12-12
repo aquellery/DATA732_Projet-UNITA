1003f408ad1273ac4e24dd78f7490ccbb4468d3a
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-12-12 13:34:54 +0100

    Recup données feuilles

--- src/lecture_excel.py
+++ src/lecture_excel.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
 file_name = '../data/data_entreprises.xlsx'  # File name
-print(file_name)
 
-df = pd.read_excel(file_name) 
-# Affichage des premières lignes du DataFrame 
-print(df.head())
+def get_feuilles(nom_feuille):
+    #Le DataFrame du fichier excel
+    df = pd.read_excel(file_name) 
+    
+    #passage DataFrame format Excel
+    dico=df.to_dict()
+    return dico
+
+#print(get_feuilles('DISSEMINATION'))
+
+def get_donnees_colonne(nom_feuille, nom_colonne):
+    pass
