36855010e5febd4e3f6d8c8a1e6326f2804c8619
Axelle <160240449+aquellery@users.noreply.github.com>
2024-12-28 21:12:28 +0100

    link companies - uni - domains

--- src/map_uni_domains_companies.py
+++ src/map_uni_domains_companies.py
@@ -3,7 +3,6 @@ from dash import dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
 from lecture_excel import get_feuilles
 from lecture_excel import get_donnees_colonne
 import liste_contacts
@@ -97,6 +96,12 @@ def filter_uni_by_domain(selected_domains, data):
         filtered=data
     return filtered
 
+def get_companies_info():
+    comanies_info=get_donnees_colonne("spinoffs contacts", ['PARTNER (SELECT)', 'NAME OF THE SPIN-OFF', 'WEBSITE', 'CONTACT PERSON', 'CONTACT MAIL', 'S3 LINKED SECTOR (Select)', 'ACTIVITY OF THE SPIN-OFF (explain)'])
+    df_companies=pd.DataFrame(comanies_info)
+    return df_companies
+
+
 df_infos, centre_lat, centre_lon=get_uni_info()
 regions_domain_dict=uni_region_domain()
 df_infos=associate_region_to_uni(df_infos, regions_domain_dict)
@@ -124,26 +129,42 @@ data_contacts=pd.DataFrame(info_contact)
 
 # handle name mismatches
 correspondance={
-    "HES-SO": "Haute-école spécialisée de Suisse occidentale"
+    "HES-SO": "Haute-école spécialisée de Suisse occidentale",
+    "Université Savoie Mont Blanc": "Université de Savoie Mont Blanc"
+
 }
 data_contacts['Institution Name']=data_contacts['Institution Name'].replace(correspondance)
 all_data=pd.merge(data, data_contacts, on='Institution Name', how='left')
 
+df_companies=get_companies_info()
+df_companies['PARTNER (SELECT)']=df_companies['PARTNER (SELECT)'].replace(correspondance)
+print(df_companies)
+
 app=dash.Dash(__name__)
 
 app.layout=html.Div([
-    html.H1("Map of Universities and Companies by Domain", style={'text-align': 'center'}),
+    dcc.Location(id='url', refresh=False),  #for navigation and updating the URL
+    html.H1("Map of Universities by Domain", style={'text-align': 'center'}),
 
-    dcc.Dropdown(
-        id='domain_choice',
-        options=[{'label': 'All', 'value': 'All'}] + [{'label': i, 'value': i} for i in data_domain],
-        placeholder="Select a domain",
-        multi=True
-    ),
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='domain_choice',
+                options=[{'label': 'All', 'value': 'All'}] + [{'label': i, 'value': i} for i in data_domain],
+                placeholder="Select a domain",
+                multi=True
+            ),
 
-    dcc.Graph(
-        id='map_display'
-    )
+            dcc.Graph(
+                id='map_display'
+            )
+        ], style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'top'}),  # Carte à gauche
+
+        html.Div([
+            html.Div(id='university_details', style={'margin-left': '20px'})  # Infos de l'université à droite
+        ], style={'width': '28%', 'display': 'inline-block', 'vertical-align': 'top'})  # Détails à droite
+    ], style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-between'})
+
 ])
 
 #Callback based on the selected domain
@@ -166,8 +187,8 @@ def update_map(selected_domains):
             filtered_data,
             lat='latitude',
             lon='longitude',
-            text='English Name',
-            color='English Name',
+            text='Institution Name',
+            color='Institution Name',
             zoom=3,
             title="Universities by Domains",
             hover_data={
@@ -181,8 +202,8 @@ def update_map(selected_domains):
             filtered_data,
             lat='latitude',
             lon='longitude',
-            text='English Name',
-            color='English Name',
+            text='Institution Name',
+            color='Institution Name',
             zoom=3,
             title="Universities by Domains",
             hover_data={
@@ -201,9 +222,54 @@ def update_map(selected_domains):
         mapbox_center={'lat':centre_lat, 'lon':centre_lon},
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
         dragmode="zoom",
-        hovermode='closest'
+        hovermode='closest',
+        showlegend=False, # we don't need to have the name of the universities since they are already on the map
     )
     return fig
+
+# informations about the companies that are linked to the uni
+@app.callback(
+    Output('university_details', 'children'),
+    [Input('map_display', 'clickData'), Input('domain_choice', 'value')]
+)
+def display_university_info(clickData, selected_domains):
+    if clickData is None:
+        return "Select a domain and click on a university to see the linked companies."
+
+    # name of the uni that you cliked one
+    selected_uni=clickData['points'][0]['text']
+   
+    # info on the selected uni
+    companies_info=df_companies[df_companies['PARTNER (SELECT)']==selected_uni]
+
+    # companies with the selected domain
+    if selected_domains and 'All' not in selected_domains:
+        print(selected_domains)
+        companies_info=companies_info[companies_info['S3 LINKED SECTOR (Select)'].isin(selected_domains)]
+        print(companies_info)
+
+    if companies_info.empty:
+        if selected_domains and 'All' not in selected_domains:
+            return f"No companies found for {selected_uni} in the selected domain(s): {', '.join(selected_domains)}."
+        else:
+            return f"No companies found for {selected_uni}."
+    
+    companies_details=[]
+    # informations about the companie linked to the university and the domain
+    for i, row in companies_info.iterrows():
+        company_info=html.Div([
+            html.H4(row['NAME OF THE SPIN-OFF']),
+            html.P(f"Website: {row['WEBSITE']}" if pd.notna(row['WEBSITE']) else "Website: Unable"),
+            html.P(f"Contact: {row['CONTACT PERSON']} ({row['CONTACT MAIL']})"),
+            html.P(f"Activity: {row['ACTIVITY OF THE SPIN-OFF (explain)']}")
+        ], style={'margin-bottom': '20px'})
+
+        companies_details.append(company_info)
+
+    return html.Div([
+        html.H4(f"Companies linked to {selected_uni} : "),
+        html.Div(companies_details)
+    ])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
